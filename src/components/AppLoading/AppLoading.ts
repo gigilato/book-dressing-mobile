9b1e0d9807f9d41a8d6a94ab606e6d1a533c2b41
gigilato <gislain.moreira@gmail.com>
2020-11-24 21:41:37 +0100

    add poppins font

--- src/components/AppLoading/AppLoading.ts
+++ src/components/AppLoading/AppLoading.ts
@@ -1,8 +1,9 @@
 import { memo, useEffect } from 'react'
 import * as SplashScreen from 'expo-splash-screen'
-// import * as Font from 'expo-font'
+import * as Font from 'expo-font'
 // import { Asset } from 'expo-asset'
 import { i18n, firebase, auth, navigation } from '@services'
+import { fonts } from '@assets'
 import { AppLoadingProps } from './AppLoading.props'
 
 SplashScreen.preventAutoHideAsync().catch(() => null)
@@ -10,7 +11,7 @@ SplashScreen.preventAutoHideAsync().catch(() => null)
 export const AppLoading = memo<AppLoadingProps>(({ onFinishLoading }) => {
   useEffect(() => {
     Promise.all([
-      // Font.loadAsync(fonts),
+      Font.loadAsync(fonts),
       // Asset.loadAsync(Object.values(images)),
       i18n.setup(),
       firebase.setup(),
